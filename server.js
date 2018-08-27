c597d40e12bf09a82a7b819f05c2573d8b4035da
doug <drcox23@gmail.com>
2018-08-26 16:34:14 -1000

    express js branch

--- server.js
+++ server.js
@@ -1,3 +1,101 @@
 const http = require("http");
 const fs = require("fs");
-const querystring = require("querystring");
+const qs = require("querystring");
+const PORT = process.env.PORT || 6969;
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET") {
+    const reqURL = req.url === "/" ? "index.html" : reqURL;
+    fs.readFile("./public" + reqURL, (err, data) => {
+      if (err) {
+        fs.readFile("./public/404.html", "utf8", (err, data) => {
+          res.write(data);
+          res.end();
+        });
+      } else {
+        res.setHeader(
+          "Content-Type",
+          `text/${reqURL.substring(path.lastIndexOf(".") + 1)}`
+        );
+        res.setHeader("Content-Length", data.length);
+        res.writeHead(200, { "content-type": "text/html" });
+        res.write(data);
+        res.end();
+      }
+    });
+  }
+  const express = require("express");
+  const app = express();
+  const PORT = process.env.PORT || 8089;
+  const qs = require("querystring");
+  const bp = require("body-parser");
+  const fs = require("fs");
+
+  app.use((req, res, next) => {
+    console.log(`${req.method} request at: ${req.url}`);
+    next();
+  });
+
+  app.use(bp.urlencoded({ extended: true }));
+
+  app.use(express.static("public"));
+
+  // app.get('/', (req, res) => {
+  //   res.sendFile(__dirname + '/public/index.html');
+  // });
+
+  // app.get('/css/styles.css', (req, res) => {
+  //   res.sendFile(__dirname + '/public/styles.css');
+  // });
+
+  // app.get('/:element', (req, res) => {
+  //   const { element } = req.params;
+  //   res.sendFile(__dirname + `/public/${element}`);
+  // });
+
+  // app.get('/hydrogen.html', (req, res) => {
+  //   res.sendFile(__dirname + '/public/hydrogen.html');
+  // });
+
+  // app.get('/helium.html', (req, res) => {
+  //   res.sendFile(__dirname + '/public/helium.html');
+  // });
+
+  app.post("/elements", (req, res) => {
+    console.log("BODY INSIDE OF POST ROUTE HANDLER", req.body);
+    const {
+      elementName,
+      elementSymbol,
+      elementDescription,
+      elementNumber
+    } = req.body;
+
+    const markup = `<!DOCTYPE html>
+                    <html lang="en">
+                    <head>
+                      <meta charset="UTF-8">
+                      <title>The Elements - ${elementName}</title>
+                      <link rel="stylesheet" href="/css/styles.css">
+                    </head>
+                    <body>
+                      <h1>${elementName}</h1>
+                      <h2>${elementSymbol}</h2>
+                      <h3>Atomic number ${elementNumber}</h3>
+                      <p>${elementDescription}</p>
+                      <p><a href="/">back</a></p>
+                    </body>
+                    </html>`;
+
+    fs.writeFile(`./public/${elementName}.html`, markup, err => {
+      res.sendFile(__dirname + `/public/${elementName}.html`);
+    });
+  });
+
+  app.get("*", (req, res) => {
+    res.sendFile(__dirname + "/public/404.html");
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server has starting on port: ${PORT}`);
+  });
+});
